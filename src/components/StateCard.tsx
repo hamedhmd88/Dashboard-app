d5b1a8c54e48aa9c0f592f9d92452dca7d436722
Hamed Mahjoobi <hmdhamed88@gmail.com>
2025-07-31 10:05:21 +0330

    update global css

--- src/components/StateCard.tsx
+++ src/components/StateCard.tsx
@@ -1,4 +1,35 @@
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
+
+// type StateCardProps = {
+//   name: string;
+//   icon: React.ElementType;
+//   value: string;
+// };
+
+// function StateCard({ name, icon: Icon, value }: StateCardProps) {
+//   return (
+//     <motion.div
+//       whileHover={{ y: -5, boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.5)" }}
+//       className="bg-[var(--component-bg)] backdrop-blur-md overflow-hidden shadow-lg rounded-xl border-[var(--border)]"
+//     >
+//       <div className=" px-4 py-5 sm:p-6">
+//         <span className=" flex items-center text-base font-medium text-[var(--text-secondary)] tracking-wide drop-shadow-md">
+//           {Icon && <Icon size={30} className="ml-4" />}
+//           <span className="ml-2 text-lg">{name}</span>
+//         </span>
+//         <p className=" mt-1 text-3xl font-semibold text-[var(--foreground)]">
+//           {value}
+//         </p>
+//       </div>
+//     </motion.div>
+//   );
+// }
+
+// export default StateCard;
+
+
+import { memo } from 'react';
+import { motion } from 'framer-motion';
 
 type StateCardProps = {
   name: string;
@@ -6,23 +37,23 @@ type StateCardProps = {
   value: string;
 };
 
-function StateCard({ name, icon: Icon, value }: StateCardProps) {
+const StateCard = memo(({ name, icon: Icon, value }: StateCardProps) => {
   return (
     <motion.div
       whileHover={{ y: -5, boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.5)" }}
-      className="bg-[var(--component-bg)] backdrop-blur-md overflow-hidden shadow-lg rounded-xl border-[var(--border)]"
+      className="dashboard-card overflow-hidden p-4 py-5 sm:p-6"
     >
-      <div className=" px-4 py-5 sm:p-6">
-        <span className=" flex items-center text-base font-medium text-[var(--text-secondary)] tracking-wide drop-shadow-md">
-          {Icon && <Icon size={30} className="ml-4" />}
-          <span className="ml-2 text-lg">{name}</span>
-        </span>
-        <p className=" mt-1 text-3xl font-semibold text-[var(--foreground)]">
-          {value}
-        </p>
-      </div>
+      <span className="flex items-center text-base font-medium text-[var(--text-secondary)] tracking-wide drop-shadow-md">
+        <Icon size={30} className="ml-4" />
+        <span className="ml-2 text-lg">{name}</span>
+      </span>
+      <p className="mt-1 text-3xl font-semibold text-[var(--foreground)]">
+        {value}
+      </p>
     </motion.div>
   );
-}
+});
+
+StateCard.displayName = 'StateCard';
 
 export default StateCard;
